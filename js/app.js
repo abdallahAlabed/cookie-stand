09cf85820166c63c95b3bf132f7faabeb9c73739
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-12 15:12:15 +0300

    lab 6

--- js/app.js
+++ js/app.js
@@ -101,11 +101,55 @@ const Lima = {
   },
 };
 
+function render(city,o) {
+  let container = document.getElementById(city);
+  
+  let locationName = document.createElement('h2');
+  
+  container.appendChild(locationName);
+  
+  locationName.textContent = city;
+  
+  let unorderedList = document.createElement('ul');
+  
+  container.appendChild(unorderedList);
+  
+  
+  let amPm = '';
+  
+   let list = '';
+  
+  let  total = 0;
+  
+  let clock = 5;
+  
+  
+  for (let i = 0; i < 15; i++) {
+    list = document.createElement('li');
+    unorderedList.appendChild(list);
+    if (clock < 12) {
+      clock++;
+    } else {
+      clock = 1;
+    }
+    if (clock < 12 && i < 6) {
+      amPm = 'am';
+    }
+    if (i >= 6) {
+      amPm = 'pm';
+    }
+    if (i < 14) {
+      list.textContent = `${clock} ${amPm}: ${o.cookPurchEachHr[i]}`;
+      total = total + o.cookPurchEachHr[i];
+    } else {
+      list.textContent = `total: ${total}`;
+    }
+  }
+  }
 
 
 
-
-
+ 
 Tokyo.avgCust();
 
 Seattle.avgCust();
@@ -116,219 +160,15 @@ Paris.avgCust();
 
 Lima.avgCust();
 
+render('Seattle',Seattle);
+  
+render('Tokyo',Tokyo);
 
-let containerSeattle = document.getElementById('Seattle');
+render('Dubai',Dubai);
 
-let locationNameSeattle = document.createElement('h2');
+render('Paris',Paris);
 
-containerSeattle.appendChild(locationNameSeattle);
-
-locationNameSeattle.textContent = 'Seattle';
-
-let unorderedListSeattle = document.createElement('ul');
-
-containerSeattle.appendChild(unorderedListSeattle);
-
-let amPm = '';
-
-let list = '';
-
-let total = 0;
-
-let clock = 5;
-
-for (let i = 0; i < 15; i++) {
-
-  list = document.createElement('li');
-  unorderedListSeattle.appendChild(list);
-  if (clock < 12) {
-    clock++;
-  } else {
-    clock = 1;
-  }
-  if (clock < 12 && i < 6) {
-    amPm = 'am';
-  }
-  if (i >= 6) {
-    amPm = 'pm';
-  }
-  if (i < 14) {
-    list.textContent = `${clock} ${amPm}: ${Seattle.cookPurchEachHr[i]}`;
-    total = total + Seattle.cookPurchEachHr[i];
-  } else {
-    list.textContent = `total: ${total}`;
-  }
-}
+render('Lima',Lima);
 
 
-let containerTokyo = document.getElementById('Tokyo');
-
-let locationNameTokyo = document.createElement('h2');
-
-containerTokyo.appendChild(locationNameTokyo);
-
-locationNameTokyo.textContent = 'Tokyo';
-
-let unorderedListTokyo = document.createElement('ul');
-
-containerTokyo.appendChild(unorderedListTokyo);
-
-
- amPm = '';
-
- list = '';
-
- total = 0;
-
- clock = 5;
-
-
-for (let i = 0; i < 15; i++) {
-  list = document.createElement('li');
-  unorderedListTokyo.appendChild(list);
-  if (clock < 12) {
-    clock++;
-  } else {
-    clock = 1;
-  }
-  if (clock < 12 && i < 6) {
-    amPm = 'am';
-  }
-  if (i >= 6) {
-    amPm = 'pm';
-  }
-  if (i < 14) {
-    list.textContent = `${clock} ${amPm}: ${Tokyo.cookPurchEachHr[i]}`;
-    total = total + Tokyo.cookPurchEachHr[i];
-  } else {
-    list.textContent = `total: ${total}`;
-  }
-}
-
-let containerDubai = document.getElementById('Dubai');
-
-let locationNameDubai = document.createElement('h2');
-
-containerDubai.appendChild(locationNameDubai);
-
-locationNameDubai.textContent = 'Dubai';
-
-let unorderedListDubai = document.createElement('ul');
-
-containerDubai.appendChild(unorderedListDubai);
-
-amPm = '';
-
- list = '';
-
- total = 0;
-
- clock = 5;
-
-for (let i = 0; i < 15; i++) {
-  list = document.createElement('li');
-  unorderedListDubai.appendChild(list);
-  if (clock < 12) {
-    clock++;
-  } else {
-    clock = 1;
-  }
-  if (clock < 12 && i < 6) {
-    amPm = 'am';
-  }
-  if (i >= 6) {
-    amPm = 'pm';
-  }
-  if (i < 14) {
-    list.textContent = `${clock} ${amPm}: ${Dubai.cookPurchEachHr[i]}`;
-    total = total + Dubai.cookPurchEachHr[i];
-  } else {
-    list.textContent = `total: ${total}`;
-  }
-}
-
-
-let containerParis = document.getElementById('Paris');
-
-let locationNameParis = document.createElement('h2');
-
-containerParis.appendChild(locationNameParis);
-
-locationNameParis.textContent = 'Paris';
-
-let unorderedListParis = document.createElement('ul');
-
-containerParis.appendChild(unorderedListParis);
-
-amPm = '';
-
- list = '';
-
- total = 0;
-
- clock = 5;
-
-for (let i = 0; i < 15; i++) {
-  list = document.createElement('li');
-  unorderedListParis.appendChild(list);
-  if (clock < 12) {
-    clock++;
-  } else {
-    clock = 1;
-  }
-  if (clock < 12 && i < 6) {
-    amPm = 'am';
-  }
-  if (i >= 6) {
-    amPm = 'pm';
-  }
-  if (i < 14) {
-    list.textContent = `${clock} ${amPm}: ${Paris.cookPurchEachHr[i]}`;
-    total = total + Paris.cookPurchEachHr[i];
-  } else {
-    list.textContent = `total: ${total}`;
-  }
-}
-
-
-let containerLima = document.getElementById('Lima');
-
-let locationNameLima = document.createElement('h2');
-
-containerLima.appendChild(locationNameLima);
-
-locationNameLima.textContent = 'Lima';
-
-let unorderedListLima = document.createElement('ul');
-
-containerLima.appendChild(unorderedListLima);
-
-amPm = '';
-
- list = '';
-
- total = 0;
-
- clock = 5;
-for (let i = 0; i < 15; i++) {
-  list = document.createElement('li');
-  unorderedListLima.appendChild(list);
-  if (clock < 12) {
-    clock++;
-  } else {
-    clock = 1;
-  }
-  if (clock < 12 && i < 6) {
-    amPm = 'am';
-  }
-  if (i >= 6) {
-    amPm = 'pm';
-  }
-  if (i < 14) {
-    list.textContent = `${clock} ${amPm}: ${Lima.cookPurchEachHr[i]}`;
-    total = total + Lima.cookPurchEachHr[i];
-  } else {
-    list.textContent = `total: ${total}`;
-  }
-}
 
