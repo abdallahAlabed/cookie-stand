cdcfc4011bf27b4423b2c9032ec896c8c7c2806d
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-14 18:25:16 +0300

    labe 09

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 let operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-let sum = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0];
+let oop = [];
+let sum = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let container = document.getElementById('tableContainer');
 let tabel = document.createElement('table');
 container.appendChild(tabel);
-let cookieShopForm = document.getElementById('add-shop-form');
+let CityForm = document.getElementById('add-shop-form');
 
 
 function City(name, minCust, maxCus, avgCookie) {
@@ -16,46 +17,7 @@ function City(name, minCust, maxCus, avgCookie) {
   this.avgCustPerHr = [];
   this.cookPurchEachHr = [];
   this.sum = 0;
-  console.log(this);
-}
-
-function randomValue(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function heder() {
-
-  let tr = document.createElement('tr');
-  tabel.appendChild(tr);
-  let th = document.createElement("th");
-    tr.appendChild(th);
-    th.textContent = 'location';
-  for (let i = 0; i < operationHour.length; i++) {
-    let th = document.createElement("th");
-    tr.appendChild(th);
-    th.textContent = operationHour[i];
-  }
-  let total = document.createElement("th");
-  tr.appendChild(total);
-  total.textContent = 'total';
-}
-
-function footr() {
-  let tr = document.createElement('tr');
-  tabel.appendChild(tr);
-  let total = document.createElement("th");
-  tr.appendChild(total);
-  total.textContent = 'total';
-  let x = 0;
-  for (let i = 0; i < sum.length; i++) {
-    let td = document.createElement("th");
-    tr.appendChild(td);
-    td.textContent = sum[i];
-    x += sum[i];
-  }
-  let td = document.createElement("th");
-  tr.appendChild(td);
-  td.textContent = x;
+  oop.push(this);
 }
 City.prototype.render = function () {
   let row = document.createElement('tr');
@@ -79,29 +41,67 @@ City.prototype.avgCust = function () {
     this.sum += this.avgCustPerHr[i];
     sum[i] += this.avgCustPerHr[i];
   }
-  console.log(sum);
 }
-
+function renderallCitys() {
+  heder();
+  for (var i in oop) {
+    oop[i].avgCust();
+    oop[i].render();
+  }
+  footr();
+}
+function randomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function heder() {
+  let tr = document.createElement('tr');
+  tabel.appendChild(tr);
+  let th = document.createElement("th");
+  tr.appendChild(th);
+  th.textContent = 'location';
+  for (let i = 0; i < operationHour.length; i++) {
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = operationHour[i];
+  }
+  let total = document.createElement("th");
+  tr.appendChild(total);
+  total.textContent = 'total';
+}
+function footr() {
+  let tr = document.createElement('tr');
+  tabel.appendChild(tr);
+  let total = document.createElement("th");
+  tr.appendChild(total);
+  total.textContent = 'total';
+  let x = 0;
+  for (let i = 0; i < sum.length; i++) {
+    let td = document.createElement("th");
+    tr.appendChild(td);
+    td.textContent = sum[i];
+    x += sum[i];
+  }
+  let td = document.createElement("th");
+  tr.appendChild(td);
+  td.textContent = x;
+}
+function addNewCity(event) {
+  event.preventDefault();
+  let loction = event.target.shopLocation.value;
+  let newMinCust = parseInt(event.target.minCust.value);
+  let newMaxCust = parseInt(event.target.maxCust.value);
+  let newCookiesPerSale = parseInt(event.target.cookiesPerSale.value);
+  let x = new City(loction, newMinCust, newMaxCust, newCookiesPerSale);
+  tabel.innerHTML = '';
+  heder();
+  renderallCitys();
+  footr();
+}
+CityForm.addEventListener('submit', addNewCity);
 let seattel = new City('seattel', 23, 65, 6.3);
 let Tokyo = new City('Tokyo', 3, 24, 1.2);
 let Dubai = new City('Dubai', 11, 38, 3.7);
 let Paris = new City('Paris', 20, 38, 2.3);
 let Lima = new City('Lima', 2, 16, 4.6);
-
-heder();
-
-
-seattel.avgCust()
-seattel.render();
-Tokyo.avgCust();
-Tokyo.render();
-Dubai.avgCust();
-Dubai.render();
-Paris.avgCust();
-Paris.render();
-Lima.avgCust();
-Lima.render();
-
-
-
-footr();
+renderallCitys();
+console.log(oop)
